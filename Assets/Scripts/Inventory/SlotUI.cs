c202a48b763b1544a61e4f7c55ecb1d83189f9a1
Emmanuella Dasilva <Emmanuella240698@gmail.com>
2024-07-17 19:42:39 -0400

    Update SlotUI.cs
    changed use and remove click events to require double clicks

--- Assets/Scripts/Inventory/SlotUI.cs
+++ Assets/Scripts/Inventory/SlotUI.cs
@@ -10,6 +10,8 @@ public class SlotUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
 {
     public Slot slot;
     public AudioSource clickSound;
+    private float lastClickTime = 0f; // Field to store the timestamp of the last click
+    private const float doubleClickThreshold = 0.3f; // Threshold in seconds
 
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -23,22 +25,27 @@ public class SlotUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (eventData.button == PointerEventData.InputButton.Left)
+        if (Time.time - lastClickTime < doubleClickThreshold)
         {
-            slot?.OnLeftClick();
-            if (clickSound != null)
+
+            if (eventData.button == PointerEventData.InputButton.Left)
             {
-                clickSound.Play();
+                slot?.OnLeftClick();
+                if (clickSound != null)
+                {
+                    clickSound.Play();
+                }
+            }
+            else if (eventData.button == PointerEventData.InputButton.Right)
+            {
+                slot?.OnRightClick();
+                if (clickSound != null)
+                {
+                    clickSound.Play();
+                }
             }
         }
-        else if (eventData.button == PointerEventData.InputButton.Right)
-        {
-            slot?.OnRightClick();
-            if (clickSound != null)
-            {
-                clickSound.Play();
-            }
-        }
+        lastClickTime = Time.time;
     }
 }
 
